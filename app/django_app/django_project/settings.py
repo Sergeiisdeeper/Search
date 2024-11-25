78381e69500bbeab9f4685c0a3bf0dda30b24079
Sergeiisdeeper <sergeimn777@gmail.com>
2024-11-25 12:27:42 +0000

    Save current progress

--- app/django_app/django_project/settings.py
+++ app/django_app/django_project/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    'main',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'main',  # или название папки приложения
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
